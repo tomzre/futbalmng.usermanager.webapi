d67e220ade17d575487f7be6f131d914d3f2d982
Tomasz Repel <trepel@andea.com>
2018-04-21 18:32:11 +0200

    Create domain models resolve

---
+++ UserManager.Core/Domain/Avatar.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace UserManager.Core.Domain
+{
+    public class Avatar
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Link { get; protected set; }
+    
+        protected Avatar()
+        {
+        }
+
+        public Avatar(string link, string name)
+        {
+            Name = name;
+            Link = link;
+        }
+    }
+
+    
+}
