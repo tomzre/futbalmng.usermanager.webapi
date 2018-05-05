00856906f47ae170289654a304d5b5cf5546d5c9
Tomasz Repel <trepel@andea.com>
2018-05-05 12:38:16 +0200

    Add roles resolve

---
+++ UserManager.Core/Domain/Roles.cs
@@ -0,0 +1,9 @@
+namespace UserManager.Core.Domain
+{
+    public class Roles
+    {
+        public static string User => "User";
+        public static string Administrator => "Administrator";
+        public static string Guest => "Guest";
+    }
+}
